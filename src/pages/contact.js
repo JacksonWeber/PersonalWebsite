2d91fe59d9bff641bf4c2cbd4fa21e64b8437a83
Jackson Weber <weberjackson79@gmail.com>
2019-06-06 22:55:33 -0400

    Add functionality to Contact page.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,11 +2,27 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import contactStyles from "../styles/layout/contact.module.css"
+import buttonStyles from "../styles/layout/button.module.css"
 
 const Contact = () => (
   <Layout>
     <SEO title="Contact" />
     <h1>Contact</h1>
+
+    <form className={ contactStyles.contactBox } name="contact" method="POST" action={"/submission"} data-netlify="true">
+      <input type={"hidden"} name={"form-name"} value={"contact"}/>
+
+      <div>
+        <input placeholder={"Email"} className={ contactStyles.email } type="email" name="email" />
+      </div>
+      <div>
+        <textarea placeholder={"Message"} className={ contactStyles.message } name="message"/>
+      </div>
+      <div>
+        <button className={ contactStyles.buttonStyle + " " + buttonStyles.buttonStyle } type="submit">Send</button>
+      </div>
+    </form>
   </Layout>
 )
 
