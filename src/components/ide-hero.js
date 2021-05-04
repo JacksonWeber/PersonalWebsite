16a374083c1fc0c096829281893b0c8e4d5316dc
Jackson Weber <jweber@datto.com>
2021-05-03 21:37:25 -0400

    Add extra information to the ide hero.

--- src/components/ide-hero.js
+++ src/components/ide-hero.js
@@ -33,6 +33,24 @@ class IDEHero extends React.Component {
             <span style={{ color: '#86909C' }}>{ ');' }</span>
           </div>
           <div style={{ 'margin-left': '1.5rem' }}>
+            <span style={{ color: '#61AEEF' }}>{ 'printf' }</span>
+            <span style={{ color: '#86909C' }}>{ '(' }</span>
+            <span style={{ color: '#98C379' }}>{ '"I\'m a full-stack developer passionate about web technologies and UI/UX"' }</span>
+            <span style={{ color: '#86909C' }}>{ ');' }</span>
+          </div>
+          <div style={{ 'margin-left': '1.5rem' }}>
+            <span style={{ color: '#61AEEF' }}>{ 'printf' }</span>
+            <span style={{ color: '#86909C' }}>{ '(' }</span>
+            <span style={{ color: '#98C379' }}>{ '"but I also love low-level programming and systems"' }</span>
+            <span style={{ color: '#86909C' }}>{ ');' }</span>
+          </div>
+          <div style={{ 'margin-left': '1.5rem' }}>
+            <span style={{ color: '#61AEEF' }}>{ 'printf' }</span>
+            <span style={{ color: '#86909C' }}>{ '(' }</span>
+            <span style={{ color: '#98C379' }}>{ '"and have experience with ERP backend development and e-commerce"' }</span>
+            <span style={{ color: '#86909C' }}>{ ');' }</span>
+          </div>
+          <div style={{ 'margin-left': '1.5rem' }}>
             <span style={{ color: '#C176D8' }}>{ 'return ' }</span>
             <span style={{ color: '#C59262' }}>{ '0' }</span>
             <span style={{ color: '#86909C' }}>{ ';' }</span>
